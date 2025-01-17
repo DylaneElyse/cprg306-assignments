32a31bcaaea954c334342b9abb0d47b882c9eaf2
DylaneElyse <dylanecunningham@outlook.com>
2025-01-17 14:14:40 -0700

    Add component and new page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Dylane Cunningham</h1>
+      <p>
+        This is a link to my GitHub:{" "}
+        <Link href="https://github.com/DylaneElyse">DylaneElyse</Link>
+      </p>
+    </main>
+  );
+}
