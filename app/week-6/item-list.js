7fb607535f7c69fb132865bfc17e0cbb7f875b82
Dylane Cunningham <155758209+DylaneElyse@users.noreply.github.com>
2025-03-03 13:30:03 -0700

    Update week 6

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -11,6 +11,16 @@ export default function ItemList() {
     setSortBy(event.target.value);
   };
 
+  const categoryMap = {
+    "dairy": [],
+    "bakery": [],
+    "produce": [],
+    "meat": [],
+    "canned goods": [],
+    "dry goods": [],
+    "household": []
+  };
+
   const sortItems = () => {
     if (sortBy === "name") {
       sortedItems.sort((a, b) => a.name.localeCompare(b.name));
@@ -18,12 +28,48 @@ export default function ItemList() {
     if (sortBy === "quantity") {
       sortedItems.sort((a, b) => a.quantity - b.quantity);
     }
+    if (sortBy === "category") {
+      sortedItems.reduce((acc, item) => {
+        const department = Object.entries(categoryMap).find(
+          ([_, items]) => items.includes(item)
+        )?.[0];
+        
+        acc[department] = acc[department] || [];
+        acc[department].push(item);
+        return acc;
+      }, {});
+    }
     return sortedItems;
   };
 
+  const SimpleDisplay = () => {
+    <ul className="text-lg">
+    {sortItems().map((item) => (
+      <li className="bg-rose-200 mb-2 py-2 px-4 rounded" key={item.id}>
+        <p className="font-semibold text-xl capitalize">{item.name}</p>
+        <p>{item.quantity}</p>
+        <p className="capitalize">{item.category}</p>
+      </li>
+    ))}
+  </ul>
+  };
+
+  const CategoryDisplay = () => {
+    <ul className="text-lg">
+    {sortItems().map((item) => (
+      <li className="bg-rose-200 mb-2 py-2 px-4 rounded" key={item.id}>
+        <p className="font-semibold text-xl capitalize">{item.name}</p>
+        <p>{item.quantity}</p>
+        <p className="capitalize">{item.category}</p>
+      </li>
+    ))}
+  </ul>
+  };
+
   return (
-    <div>
-      <button
+    <div className="flex flex-col items-center">
+      <div className="pb-3">
+        <button
         value={"name"}
         className="bg-rose-400 rounded text-neutral-50 font-bold text-2xl text-center m-1 p-2"
         onClick={(event) => handleSort(event)}
@@ -37,16 +83,25 @@ export default function ItemList() {
       >
         Quantity
       </button>
+      {/* <button
+        value={"category"}
+        className="bg-rose-400 rounded text-neutral-50 font-bold text-2xl text-center m-1 p-2"
+        onClick={(event) => handleSort(event)}
+      >
+        Category
+      </button> */}
+
+      </div>
 
       <ul className="text-lg">
-        {sortItems().map((item) => (
-          <li className="bg-rose-200 w-1/5 mb-2 p-4 rounded" key={item.id}>
-            <p className="font-semibold text-xl">{item.name}</p>
-            <p>{item.quantity}</p>
-            <p>{item.category}</p>
-          </li>
-        ))}
-      </ul>
+      {sortItems().map((item) => (
+      <li className="bg-rose-200 mb-2 py-2 px-4 rounded" key={item.id}>
+        <p className="font-semibold text-xl capitalize">{item.name}</p>
+        <p>{item.quantity}</p>
+        <p className="capitalize">{item.category}</p>
+      </li>
+    ))}
+  </ul>
     </div>
   );
 }
--- app/week-6/page.js
+++ app/week-6/page.js
@@ -4,8 +4,8 @@ import ItemList from "./item-list";
 
 export default function Page() {
   return (
-    <main>
-      <h1 className="font-bold text-4xl ml-4 pb-5 pt-2">Shopping List</h1>
+    <main className="flex items-center place-content-center w-1/2 flex-col">
+      <h1 className="font-bold text-4xl pb-5 pt-2">Shopping List</h1>
       <ItemList />
     </main>
   );
